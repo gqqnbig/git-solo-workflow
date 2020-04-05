38f234232332ce8eed1928a6763483fe37404482
QQ喵 <gqqnb2005@gmail.com>
2020-04-04 20:54:49 -0700

    可以发起pull request

--- solidCommits.py
+++ solidCommits.py
@@ -1,11 +1,10 @@
-import os
 import re
 import sys
 
-from git import Repo, GitCommandError
+import json
+import requests
 
-# A commit is considered as stable if there are this number of commits after it.
-STABLE_COMMIT_AGE = 10
+from git import Repo, GitCommandError
 
 
 def getPushUrl():
@@ -15,11 +14,10 @@ def getPushUrl():
 		raise AssertionError(f'Expect 1 url for remote origin, but there are {len(urls)}.')
 	originUrl = urls[0]
 	assert originUrl.startswith('https://github.com')
-	pushUrl = f"https://{GITHUB_TOKEN}@github.com{originUrl[len('https://github.com'):]}"
 	return pushUrl
 
 
-def makePullRequest(branchName, commits):
+def makeIssueBranch(branchName, commits):
 	try:
 		repo.create_head(branchName, 'master')
 		repo.git.checkout(branchName, force=True)
@@ -31,13 +29,80 @@ def makePullRequest(branchName, commits):
 	repo.git.push(pushUrl, branchName)
 
 
+def makePullRequest(token, repositoryId, branchName, title):
+	body = '''
+{
+  createPullRequest(input:{
+    	repositoryId:"%s",
+    	baseRefName:"master",
+    	headRefName:"%s",
+    	title:"%s"}) {
+    pullRequest {
+      createdAt
+      id
+    }
+  }
+}''' % (repositoryId, branchName, title)
+	response = requests.post('https://api.github.com/graphql', headers={'Authorization': 'bearer ' + token}, data=json.dumps({"mutation": body}))
+	data = response.json()
+
+	return data['data']['createPullRequest']['pullRequest']
+
+
+def getRepositoryId(token, repositoryOwner, repositoryName):
+	body = '''
+{
+  repository(name:"%s", owner:"%s") {
+    id 
+  }
+}''' % (repositoryName, repositoryOwner)
+	response = requests.post('https://api.github.com/graphql', headers={'Authorization': 'bearer ' + token}, data=json.dumps({"query": body}))
+	data = response.json()
+	return data['data']['repository']['id']
+
+
+repo = None
+try:
+	repo = Repo(sys.argv[-1])
+except Exception as e:
+	print(sys.argv[-1] + " is not a valid Git repo. Append the proper path as the last parameter:\n" + str(e))
+	exit(1)
+
 try:
 	index = sys.argv.index("--token")
 	GITHUB_TOKEN = sys.argv[index + 1]
 except:
 	raise AssertionError('GitHub token is not found. Use --token option to provide it.')
 
-repo = Repo(os.path.dirname(os.path.abspath(__file__)) + '/../..')
+try:
+	index = sys.argv.index('--repositoryName')
+	repositoryName = sys.argv[index + 1]
+
+	index = sys.argv.index('--repositoryOwner')
+	repositoryOwner = sys.argv[index + 1]
+except:
+	origin = repo.remote('origin')
+	urls = list(origin.urls)
+	if len(urls) != 1:
+		raise AssertionError(f'Expect 1 url for remote origin, but there are {len(urls)}.')
+	originUrl = urls[0]
+
+	m = re.search(r'github\.com/([^/]+)/(.+?)\.git', originUrl)
+	if m is None:
+		raise AssertionError('Unable to find repository name and repository owner from git remote origin. You may want to provide it through command line options --repositoryName, --repositoryOwner.')
+
+	repositoryOwner = m.group(1)
+	repositoryName = m.group(2)
+
+repositoryId = getRepositoryId(GITHUB_TOKEN, repositoryOwner, repositoryName)
+
+# A commit is considered as stable if there are this number of commits after it.
+try:
+	index = sys.argv.index("--stable-age")
+	STABLE_COMMIT_AGE = int(sys.argv[index + 1])
+except:
+	STABLE_COMMIT_AGE = 20
+
 commits = list(repo.iter_commits('master..dev'))
 if len(commits) < STABLE_COMMIT_AGE:
 	exit(0)
@@ -46,14 +111,13 @@ commits.reverse()
 stableCommits = commits[:-STABLE_COMMIT_AGE]
 unstableCommits = commits[-STABLE_COMMIT_AGE:]
 
-repo.git.checkout('master')
 if repo.is_dirty():
 	raise AssertionError('Unable to work on a dirty repository.')
 
+repo.git.checkout('master')
 hasError = False
 stableIssues = {}
 
-pushUrl = getPushUrl()
 for commit in stableCommits:
 	issueIdMatch = re.search(r'Issue: #(\d+)', commit.message)
 	if issueIdMatch is not None:
@@ -73,8 +137,8 @@ for commit in stableCommits:
 			print(f'Commit {commit.hexsha} is stable, but cannot be applied to master.', file=sys.stderr)
 			hasError = True
 
+pushUrl = f"https://{GITHUB_TOKEN}@github.com/{repositoryOwner}/{repositoryName}.git"
 repo.git.push(pushUrl, 'master')
-
 
 for commit in unstableCommits:
 	issueIdMatch = re.search(r'Issue: #(\d+)', commit.message)
@@ -82,13 +146,21 @@ for commit in unstableCommits:
 		issueId = int(issueIdMatch.group(1))
 		stableIssues.pop(issueId, None)
 
-
 for id, commits in stableIssues.items():
 	print(f'Issue {id} (commits {commits}) is stable.')
 
 	branchName = 'Issue' + str(id)
 	try:
-		makePullRequest(branchName, commits)
+		makeIssueBranch(branchName, commits)
 	except Exception as e:
 		print(str(e), file=sys.stderr)
 		hasError = True
+
+	try:
+		print(makePullRequest(GITHUB_TOKEN, repositoryId, branchName, 'Implement issue ' + str(id)))
+	except Exception as e:
+		print(str(e), file=sys.stderr)
+		hasError = True
+
+if hasError:
+	exit(1)
