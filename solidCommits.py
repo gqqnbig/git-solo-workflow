045c742eb2e07e5cfd5e99633bcbb7379a182d98
QQ喵 <gqqnb2005@gmail.com>
2020-04-04 18:49:22 -0700

    可以推送master分支

--- solidCommits.py
+++ solidCommits.py
@@ -5,7 +5,37 @@ import sys
 from git import Repo, GitCommandError
 
 # A commit is considered as stable if there are this number of commits after it.
-STABLE_COMMIT_AGE = 1
+STABLE_COMMIT_AGE = 10
+
+
+def getPushUrl():
+	origin = repo.remote('origin')
+	urls = list(origin.urls)
+	if len(urls) != 1:
+		raise AssertionError(f'Expect 1 url for remote origin, but there are {len(urls)}.')
+	originUrl = urls[0]
+	assert originUrl.startswith('https://github.com')
+	pushUrl = f"https://{GITHUB_TOKEN}@github.com{originUrl[len('https://github.com'):]}"
+	return pushUrl
+
+
+def makePullRequest(branchName, commits):
+	try:
+		repo.create_head(branchName, 'master')
+		repo.git.checkout(branchName, force=True)
+		repo.git.cherry_pick(commits)
+
+	except GitCommandError as e:
+		raise AssertionError(f'Unable to rearrange commits {commits} based on master.\n{e}')
+
+	repo.git.push(pushUrl, branchName)
+
+
+try:
+	index = sys.argv.index("--token")
+	GITHUB_TOKEN = sys.argv[index + 1]
+except:
+	raise AssertionError('GitHub token is not found. Use --token option to provide it.')
 
 repo = Repo(os.path.dirname(os.path.abspath(__file__)) + '/../..')
 commits = list(repo.iter_commits('master..dev'))
@@ -18,11 +48,12 @@ unstableCommits = commits[-STABLE_COMMIT_AGE:]
 
 repo.git.checkout('master')
 if repo.is_dirty():
-	print('Unable to work on dirty repository.', file=sys.stderr)
-	exit(1)
+	raise AssertionError('Unable to work on a dirty repository.')
 
 hasError = False
 stableIssues = {}
+
+pushUrl = getPushUrl()
 for commit in stableCommits:
 	issueIdMatch = re.search(r'Issue: #(\d+)', commit.message)
 	if issueIdMatch is not None:
@@ -42,19 +73,22 @@ for commit in stableCommits:
 			print(f'Commit {commit.hexsha} is stable, but cannot be applied to master.', file=sys.stderr)
 			hasError = True
 
+repo.git.push(pushUrl, 'master')
+
+
 for commit in unstableCommits:
 	issueIdMatch = re.search(r'Issue: #(\d+)', commit.message)
 	if issueIdMatch is not None:
 		issueId = int(issueIdMatch.group(1))
 		stableIssues.pop(issueId, None)
 
+
 for id, commits in stableIssues.items():
 	print(f'Issue {id} (commits {commits}) is stable.')
 
+	branchName = 'Issue' + str(id)
 	try:
-		repo.create_head('Issue' + str(id), 'master')
-		repo.git.checkout('Issue' + str(id), force=True)
-		repo.git.cherry_pick(commits)
-	except GitCommandError as e:
-		print(f'Unable to rearrange commits {commits} based on master.\n{e}', file=sys.stderr)
+		makePullRequest(branchName, commits)
+	except Exception as e:
+		print(str(e), file=sys.stderr)
 		hasError = True
